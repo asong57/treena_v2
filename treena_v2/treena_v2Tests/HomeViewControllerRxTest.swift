759deb045edd077370165d74a03e00d8cf5e6f28
asong57 <1998thddk@gmail.com>
2022-04-19 16:14:58 +0900

    Feat : RxTest 예제 추가

---
+++ treena_v2/treena_v2Tests/HomeViewControllerRxTest.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewControllerRxTest.swift
+//  treena_v2Tests
+//
+//  Created by asong on 2022/04/19.
+//
+
+import Foundation
--- treena_v2/treena_v2Tests/HomeViewControllerTest.swift
+++ treena_v2/treena_v2Tests/HomeViewControllerTest.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import RxTest
 @testable import treena_v2
 
 class HomeViewControllerTest: XCTestCase {
@@ -23,10 +24,6 @@ class HomeViewControllerTest: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
-    func testHomeViewModel_WhenHomeVCAppeared_ShouldReturnCorrectTreeLevel(){
-        XCTAssertTrue(true)
-    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
@@ -34,5 +31,27 @@ class HomeViewControllerTest: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+ 
+    func testHomeViewModel_WhenHomeVCAppeared_ShouldReturnCorrectTreeLevel(){
+        // RxTest 예제 코드
+        let scheduler = TestScheduler(initialClock: 0)
+        let observable = scheduler.createHotObservable([
+            .next(210, 2),
+            .next(220, 4),
+            .completed(400)
+        ])
+        
+        let observer = scheduler.createObserver(Int.self)
+        observable.subscribe(observer)
+        
+        scheduler.start()
+        XCTAssertEqual(observer.events, [.next(210, 2),
+                                         .next(220, 4),
+                                         .completed(400)])
+        
+    }
+    
+    func testHomeViewModel_WhenHomeVCAppeared_ShouldReturnCorrectTree(){
+        XCTAssertTrue(true)
+    }
 }
